cc3cebf6b422424a8017662c44b4a2834e6fda52
John Paul Calverley <Jpcal1991@outlook.com>
2023-01-04 11:59:48 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Practise
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.chkMarried = New System.Windows.Forms.CheckBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cboAgerange = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(227, 118)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(182, 26)
+        Me.TextBox1.TabIndex = 0
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(263, 334)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(98, 35)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Arial", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Cyan
+        Me.Label1.Location = New System.Drawing.Point(221, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(203, 33)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Form Practise"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Arial", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Cyan
+        Me.Label2.Location = New System.Drawing.Point(80, 118)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(120, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Enter Name"
+        '
+        'chkMarried
+        '
+        Me.chkMarried.AutoSize = True
+        Me.chkMarried.BackColor = System.Drawing.Color.Red
+        Me.chkMarried.Font = New System.Drawing.Font("Arial", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkMarried.ForeColor = System.Drawing.Color.Cyan
+        Me.chkMarried.Location = New System.Drawing.Point(227, 171)
+        Me.chkMarried.Name = "chkMarried"
+        Me.chkMarried.Size = New System.Drawing.Size(104, 27)
+        Me.chkMarried.TabIndex = 4
+        Me.chkMarried.Text = "Married"
+        Me.chkMarried.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Cyan
+        Me.Label3.Location = New System.Drawing.Point(80, 171)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(141, 24)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Marital Status"
+        '
+        'cboAgerange
+        '
+        Me.cboAgerange.FormattingEnabled = True
+        Me.cboAgerange.Items.AddRange(New Object() {"18-22", "22-30", "31-50", "51-80"})
+        Me.cboAgerange.Location = New System.Drawing.Point(227, 217)
+        Me.cboAgerange.Name = "cboAgerange"
+        Me.cboAgerange.Size = New System.Drawing.Size(121, 28)
+        Me.cboAgerange.TabIndex = 6
+        Me.cboAgerange.Text = "Age Range"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Cyan
+        Me.Label4.Location = New System.Drawing.Point(80, 221)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(112, 24)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Age Range"
+        '
+        'Practise
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(672, 402)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cboAgerange)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.chkMarried)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Practise"
+        Me.Text = "Pratise"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents chkMarried As CheckBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cboAgerange As ComboBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Practise
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'End application
+        End
+
+    End Sub
+
+    Private Sub chkMarried_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkMarried.CheckedChanged
+
+        If chkMarried.CheckState = CheckState.Checked Then
+
+        End If
+
+    End Sub
+
+    Private Sub cboAgerange_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboAgerange.SelectedIndexChanged
+
+    End Sub
+End Class
